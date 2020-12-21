3ecef7323b77a7a71dcb6aa29280ede7293210c0
Jason Baldus <jason.baldus@gmail.com>
2020-12-21 09:32:41 -0500

    Adds questions to hello world

--- helloworld.py
+++ helloworld.py
@@ -6,4 +6,11 @@ print('| By Mr. B    |')
 print('+-------------+')
 print('')
 
-print('Hello World!')
+name = input('What is your name? ')
+age = input('What is your age? ')
+if age.isnumeric():
+    age = int(age)
+    print(f'Hello {name}. You are {age} years old')
+    print(f'Next year, you will be {age+1} years old')
+else:
+    print("You didn't type a valid number.")
